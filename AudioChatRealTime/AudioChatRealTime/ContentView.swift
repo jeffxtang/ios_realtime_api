ca2fa56bc72ae86be71a26de8d39b96cb682a1db
Jeff Tang <jeff.x.tang@gmail.com>
2025-02-21 11:19:58 -0800

    showing UI status listening after init done

--- AudioChatRealTime/AudioChatRealTime/ContentView.swift
+++ AudioChatRealTime/AudioChatRealTime/ContentView.swift
@@ -11,7 +11,8 @@ import SwiftUI
 import OpenAI
 import SwiftUI
 
-let OPENAI_API_KEY = "xxxxx"
+let OPENAI_API_KEY = "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"
+
 
 struct MessageBubble: View {
   let message: Item.Message
@@ -44,9 +45,36 @@ struct MessageBubble: View {
   }
 }
 
+struct ConversationView: View {
+    @Bindable var conversation: Conversation  
+
+    var body: some View {
+        VStack {
+            if conversation.isListening {
+                Text("Listening...")
+                .font(.headline)
+                .foregroundColor(.blue)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+
+            } else {
+                Text("Initializing...")
+                .font(.headline)
+                .foregroundColor(.blue)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+            }
+        }
+    }
+}
+
 class ConversationViewModel: ObservableObject {
   @Published var conversation: Conversation?
-    
+      
   func initWebSocket() {
         Task {
           let conversation = Conversation(authToken: OPENAI_API_KEY)
@@ -68,11 +96,10 @@ class ConversationViewModel: ObservableObject {
 }
 
 
-
 struct ContentView: View {
   @StateObject private var viewModel = ConversationViewModel()
   @State private var newMessage: String = ""
-  @State private var status: String = ""
+  @State private var status: String = "Pick one!"
   @State private var areButtonsEnabled = true
   
   var messages: [Item.Message] {
@@ -130,13 +157,17 @@ struct ContentView: View {
       }
       .padding()
       
-      Text(status)
-        .font(.headline)
-        .foregroundColor(.blue)
-        .padding()
-        .frame(maxWidth: .infinity)
-        .background(Color.gray.opacity(0.2))
-        .cornerRadius(8)
+      if let conversation = viewModel.conversation {
+          ConversationView(conversation: conversation)
+      } else {
+        Text(status)
+          .font(.headline)
+          .foregroundColor(.blue)
+          .padding()
+          .frame(maxWidth: .infinity)
+          .background(Color.gray.opacity(0.2))
+          .cornerRadius(8)
+      }
     }
     .navigationTitle("Chat")
     .navigationBarTitleDisplayMode(.inline)
